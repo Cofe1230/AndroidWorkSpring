e3a5f7317c0dba91217154ba8b604edfafc70190
Cofe1230 <winner1230@gmail.com>
2023-08-11 11:40:13 +0900

    111

---
+++ androidPerson/src/main/java/com/example/androidPerson/AndroidPersonApplication.java
@@ -0,0 +1,13 @@
+package com.example.androidPerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidPersonApplication.class, args);
+	}
+
+}
---
+++ androidPerson/src/test/java/com/example/androidPerson/AndroidPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.androidPerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
